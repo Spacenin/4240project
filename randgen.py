61dc3264225fa701fe710e0250d4e87d2713be35
Eliza <elsorber@gmail.com>
2024-04-10 14:50:04 -0400

    Added command line arguments. -u is upper bound -l is lower boud -n is number of rands. I set arbitrary defaults. will return a array of the number of rands

--- randgen.py
+++ randgen.py
@@ -2,6 +2,9 @@
 
 
 import random
+import argparse
+
+
 
 #yoooo read all values from a file and store in array
 def read_values(filename):
@@ -10,7 +13,7 @@ def read_values(filename):
     return values
 
 
-def generate_random_numbers(x_values, y_values, z_values):
+def generate_random_numbers(x_values, y_values, z_values, upper_bound, lower_bound):
     while True:
         #grabs random value from the 3 files
         x = random.choice(x_values)
@@ -20,20 +23,43 @@ def generate_random_numbers(x_values, y_values, z_values):
         seed_value = x + y + z
         random.seed(seed_value)
 
-        random_number = random.randint(0, 1000)
+        random_number = random.randint(lower_bound, upper_bound)
         return random_number
         
 
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", type=int)
+    parser.add_argument("-u", type=int)
+    parser.add_argument("-n", type=int)
+    args = parser.parse_args()
+
+    if args.l is None or args.u is None or args.n is None:
+        lower_bound = 0
+        upper_bound = 1000
+        num_rands = 1
+    else:
+        lower_bound = args.l
+        upper_bound = args.u
+        num_rands = args.n
+
     #read the files and save
     x_values = read_values('x.txt')
     y_values = read_values('y.txt')
     z_values = read_values('z.txt')
 
-    random_number = generate_random_numbers(x_values, y_values, z_values)
+    
+    rands = []
+    x = 0
+    while x < num_rands:
+        random_number = generate_random_numbers(x_values, y_values, z_values, upper_bound, lower_bound)
+        print(random_number)
+        rands.append(random_number)
+        x = x+1
 
-    print(random_number)
-    return (random_number)
+
+    return (rands)
 
 
 if __name__ == "__main__":
